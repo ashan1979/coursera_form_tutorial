7d1d4cd8b0b1c9f3d4354736699337bb1267484f
ashan <indrajee79@gmail.com>
2022-10-24 11:20:57 +0530

    html and css

---
+++ userreg.php
@@ -0,0 +1,5 @@
+<?php
+//
+// At this point... the data keyed into the form fields would be sent to the server to be processed by the program defined in the action attribute on the form element in the HTML file.
+//
+?>
